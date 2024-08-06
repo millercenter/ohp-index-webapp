b49486afbb4afe83ede4e53c81328620360d7711
Miles Efron <mefron@gmail.com>
2024-08-06 14:50:10 -0400

    testing ssm

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import boto3
+import json
+
+ssm = boto3.client('ssm')
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-   return "Hello, World!"
+    db = ssm.get_parameter(Name='/ohp-index-webapp/dbname')
+    db = db['Parameter']['Value']
+    return "Hello, World: " + db
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5002)
